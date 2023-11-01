75a64947bd779d76027c01371e06d856494a1e21
tlstnrud82 <149546448+tlstnrud82@users.noreply.github.com>
2023-11-01 09:57:42 +0900

    Add files via upload

---
+++ js/basic-slector.js
@@ -0,0 +1,15 @@
+const firstTitle = document.getE1('#title')
+console.log firstTitle = document.getElementById('title')
+console.log('title')
+console.log(firstTitle)
+//class명으로 요소 불러오기
+const myList = document.getElementsByClassName('list')
+console.log(myList)
+//태그명으로 요소 불러오기
+const myList2 = document.querySelector('list.red')
+console.log(paragraph)
+//css.상의 선택지를 다 쓰는 방법
+//방법2
+firstTitle.onclick = ()=>{
+    let temp
+}
---
+++ js/if.js
@@ -0,0 +1,55 @@
+// 조건문(if..else)
+// if(조건식) {
+//     실행문(조건문이 참일경우)
+// }else {
+//     실행문(조건식이 거짓일 경우)
+// }
+
+// 조건문(if...elseif...else)
+// let gender=true
+// if(조건식) {
+//     실행문(조건식이 참일 경우)
+// }else if(조건n){
+//     실행문(조건식이 거짓일 경우)
+// }else if(조건n일 경우)
+// {
+//    실행문(조건n이 참일 경우) 
+// }else {
+//     실행문(조건n이 거짓일 경우)
+// }
+let age=18
+let (age = prompt('당신의 나이는 몇살인가요?')
+age = promise
+if(age > 20){
+    console("운전이 가능합니다")
+}else if(age>=18 age|| age<=20) {
+    console.log("오토바이운전이 가능합니다")
+}else if(){
+    console.log("오토바이운전이 가능합니다")
+}else (age>=18){
+    console.log("운전이 불가능합니다.")
+}else {
+    console.log("운전바이운전이 가능합니다.")
+}else if(age>=18){
+
+}
+잘못된 조건문 - 조건문 순서 고려
+
+// 당신의 나이는 몇살인가요?
+// 운전면허증이 있나요?
+
+// 나이가 20세초과인경우, 운전면허가 있으면(yes) "운전가능합니다." 콘솔창에 출려하고, 그렇지 않으면 "면허를 따세요"라고 출력하고, 나이가 20세이하이면 "운전불가능합니다."라고 출력하세요
+
+let age = prompt
+let license = prompt
+let = parselInt(age)
+
+//3의 배수
+let userNumber = prompt("숫서를 입력하세요");
+if(userNumber !==null && userNumber !==""){
+    userNumber = parseInt(userNumber)
+    SpeechRecognitionAlternative
+    ("3의 배수입니다.")
+}else if(userNumber === 0){
+    
+}
